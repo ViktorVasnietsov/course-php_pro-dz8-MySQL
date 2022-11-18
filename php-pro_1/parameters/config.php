05958a7d06a430c968878ba841efb45a9afda5c9
ViktorVasnietsov <vitya072@gmail.com>
2022-11-18 10:59:24 +0200

    SQL
    Add MySQL DB

---
+++ php-pro_1/parameters/config.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'dbFile' => __DIR__ . '/../storage/db.json',
+    'monolog' => [
+        'channel' => 'general',
+        'level' => [
+            'error' => __DIR__ . '/../logs/error.log',
+            'info' => __DIR__ . '/../logs/info.log',
+        ],
+    ],
+    'urlConverter' => [
+        'codeLength' => 8,
+    ],
+];
